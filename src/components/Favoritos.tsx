b738bab8b19fe88f855b6ca9fd59c292d6b4917c
Agudrey <cristianagudelorey1@gmail.com>
2023-03-07 13:01:12 -0500

    fix bugs

--- src/components/Favoritos.tsx
+++ src/components/Favoritos.tsx
@@ -1,10 +1,60 @@
+import useLocalStorage from "use-local-storage";
+import Modal from "react-modal";
+import styles from "./Favoritos.module.css"
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
+interface ICatalogue {
+  id: number;
+  image: any;
+  price: string;
+  favorite: boolean;
+  name: string;
+}
 
+function Favoritos(props: any) {
+  const { primaryFont } = props;
+  const { modalIsOpen } = props;
+  const { closeModal } = props;
 
-const Favoritos = () => {
-  return(
-    <div></div>
+  const [catalogueFav] = useLocalStorage("CatalogueFav", "");
+
+  const Favs = JSON.parse(catalogueFav);
+
+  console.log(catalogueFav);
+  
+
+  return (
+    <div>
+      <Modal
+        className={styles.modal}
+        overlayClassName={styles.Overlay}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <div className={styles.modal_header}>
+          <h2>Mirá tus favoritos!</h2>
+          <button className={styles.close_button} onClick={closeModal}>
+            <AiOutlineCloseCircle />
+          </button>
+        </div>
+ 
+        {Favs.map((item: ICatalogue) => (
+          <div key={item.id} className={styles.fav_cards}>
+            <img
+              className={styles.fav_image}
+              alt="item image"
+              src={item.image}
+            />
+            <div>
+              <p>{item.name}</p>
+              <p>{item.price} COP</p>
+            </div>
+          </div>
+        ))}
+      </Modal>
+    </div>
   );
-};
+}
 
-export default Favoritos
+export default Favoritos;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { FiHeart } from "react-icons/fi";
 import { BiSearchAlt } from "react-icons/bi";
 import styles from "../components/Navbar.module.css";
 import { useState } from "react";
+import Favoritos from "./Favoritos";
 
 
 const Navbar = () => {
@@ -11,8 +12,17 @@ const Navbar = () => {
 
   const [mobile, setMobile] = useState(false);
   const [text, setText] = useLocalStorage('text_input', '')
+  const [modalIsOpen, setIsOpen] = useState(false);
   
-  
+  function openModal() {
+    setIsOpen(true);
+  }
+
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
 
   const clickMobile = () => {
     setMobile(!mobile);
@@ -20,64 +30,75 @@ const Navbar = () => {
 
   
   return (
-    <div className={styles.navbar}>
-      <div className={`${styles.logo_container} ${styles.logo_desktop}`}>
-        BREAK FREE
-      </div>
+    <>
+      <div className={styles.navbar}>
+        <div className={`${styles.logo_container} ${styles.logo_desktop}`}>
+          BREAK FREE
+        </div>
 
-      <div
-        className={
-          !mobile 
-            ?`${styles.logo_container} ${styles.logo_mobile}`
-            : styles.disable
-        }
-      >
-        BREAK FREE
-      </div>
-
-      <div className={styles.right_container}>
         <div
-          className={`${styles.search_container} ${styles.search_container_desktop}`}
+          className={
+            !mobile 
+              ?`${styles.logo_container} ${styles.logo_mobile}`
+              : styles.disable
+          }
         >
-          <input
-            className={`${styles.search}`}
-            type="text"
-            placeholder="Busca tus prendas favoritas, blusas, pantalones..."
-            maxLength={50}
-            onChange={(e) => setText(e.target.value)}
+          BREAK FREE
+        </div>
+
+        <div className={styles.right_container}>
+          <div
+            className={`${styles.search_container} ${styles.search_container_desktop}`}
+          >
+            <input
+              className={`${styles.search}`}
+              type="text"
+              placeholder="Busca tus prendas favoritas, blusas, pantalones..."
+              maxLength={50}
+              onChange={(e) => setText(e.target.value)}
             
-          />
+            />
 
-          <button className={styles.search_button}>
-            <BiSearchAlt />
+            <button className={styles.search_button}>
+              <BiSearchAlt />
+            </button>
+          </div>
+
+          <div
+            className={`${styles.search_container} ${styles.search_container_mobile}`}
+          >
+            <input
+              className={
+                mobile
+                  ?`${styles.search} ${styles.search_mobile}`
+                  : styles.disable
+              }
+              type="text"
+              placeholder="Busca tus prendas favoritas, blusas, pantalones..."
+              maxLength={30}
+              onChange={(e) => setText(e.target.value)}
+            />
+
+            <button onClick={clickMobile} className={styles.search_button}>
+              <BiSearchAlt />
+            </button>
+          </div>
+
+          <button 
+            className={styles.like_button}
+            onClick={openModal}
+          >
+            <FiHeart />
           </button>
         </div>
-
-        <div
-          className={`${styles.search_container} ${styles.search_container_mobile}`}
-        >
-          <input
-            className={
-              mobile
-                ?`${styles.search} ${styles.search_mobile}`
-                : styles.disable
-            }
-            type="text"
-            placeholder="Busca tus prendas favoritas, blusas, pantalones..."
-            maxLength={30}
-            onChange={(e) => setText(e.target.value)}
-          />
-
-          <button onClick={clickMobile} className={styles.search_button}>
-            <BiSearchAlt />
-          </button>
-        </div>
-
-        <button className={styles.like_button}>
-          <FiHeart />
-        </button>
       </div>
-    </div>
+
+      <Favoritos 
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+      />
+
+    </>
   );
 };
 
